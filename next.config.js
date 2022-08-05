a2e81f6f2e63ecdec3d0713b3c4368684200a77e
Ferdian9991 <ferdisatria15@gmail.com>
2022-08-05 16:33:36 +0700

    test request

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = withBundleAnalyzer({
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  compilerOptions: {
+    jsxImportSource: "preact",
+  },
+  reactStrictMode: false,
+});
+
+module.exports = nextConfig;
