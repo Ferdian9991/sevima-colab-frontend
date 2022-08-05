fd84be5ac8ed901ebc2fc5e6db4af3f685c21ca7
Ferdian9991 <ferdisatria15@gmail.com>
2022-08-05 15:41:27 +0700

    add babel tailwindcss

---
+++ .babelrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: [
+    [
+      "next/babel",
+      { "preset-react": { runtime: "automatic" } },
+    ],
+  ],
+  plugins: [
+    "babel-plugin-macros",
+    [
+      "styled-components",
+      {
+        ssr: false,
+        displayName: false,
+        minify: true,
+        transpileTemplateLiterals: true,
+      },
+    ],
+  ],
+};
